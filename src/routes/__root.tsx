7c66f723ce09fde90c6d080aeaa90173df3aec3e
Saif-ur-rehman <106364035+saifurrehman022@users.noreply.github.com>
2026-06-25 20:58:22 +0500

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,14 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import {
+  createRootRoute,
+  Outlet,
+} from "@tanstack/react-router";
 
 export const Route = createRootRoute({
-  component: () => <Outlet />,
+  component: RootComponent,
 });
+
+function RootComponent() {
+  return (
+    <Outlet />
+  );
+}
