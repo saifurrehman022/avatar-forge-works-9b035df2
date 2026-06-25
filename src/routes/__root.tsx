62d185a563a14513bd0ac17c843866a822a02cc0
Saif-ur-rehman <106364035+saifurrehman022@users.noreply.github.com>
2026-06-25 20:44:02 +0500

    Create __root.tsx

---
+++ src/routes/__root.tsx
@@ -0,0 +1,19 @@
+import {
+  Outlet,
+  createRootRoute,
+} from "@tanstack/react-router";
+
+import { Toaster } from "@/components/ui/sonner";
+
+export const Route = createRootRoute({
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <>
+      <Outlet />
+      <Toaster />
+    </>
+  );
+}
