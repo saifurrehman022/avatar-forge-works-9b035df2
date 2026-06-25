90880e70eed23497b74474f2cf73e4efbadc2844
Saif-ur-rehman <106364035+saifurrehman022@users.noreply.github.com>
2026-06-25 18:02:18 +0500

    Create privacy.tsx

---
+++ src/routes/privacy.tsx
@@ -0,0 +1,9 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/privacy")({
+  component: PrivacyPage,
+});
+
+function PrivacyPage() {
+  return <div>Privacy Policy for Avatar Forge</div>;
+}
